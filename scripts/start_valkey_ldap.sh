a3458b001b5a9e8fe8d251e9809bb3aa86451b50
Ricardo Dias <ricardo.dias@percona.com>
2025-03-21 10:53:14 +0000

    Run unit tests in CI workflow
    Signed-off-by: Ricardo Dias <ricardo.dias@percona.com>

--- scripts/start_valkey_ldap.sh
+++ scripts/start_valkey_ldap.sh
@@ -12,19 +12,24 @@ if [ ! -z $DOCKER_COMPOSE_RUNNING ]; then
 else
     pushd scripts/docker > /dev/null
 
-    docker compose up > /tmp/valkey-ldap.log 2>&1 &
-    DOCKER_COMPOSE_PID=$!
+    docker compose up -d --wait
+    docker compose logs -f > /tmp/valkey-ldap.log 2>&1 &
 
     popd > /dev/null
 fi
 
 # Wait for valkey-server to be online
 while true; do
+    echo "Waiting for Valkey server"
+    sleep 1
     nc -z localhost 6379 && break
 done
 
 # Wait for ldap server to be online
 while true; do
+    echo "Waiting for LDAP server"
+    sleep 1
     nc -z localhost 389 && break
 done
 
+./scripts/populate_ldap.sh
