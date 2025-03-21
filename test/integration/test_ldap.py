5063b0f9242315a90453f7d708864bcb61ac99a2
Ricardo Dias <ricardo.dias@percona.com>
2025-03-21 16:56:18 +0000

    Add integration test that calls `ldap.test_auth` command
    Signed-off-by: Ricardo Dias <ricardo.dias@percona.com>

--- test/integration/test_ldap.py
+++ test/integration/test_ldap.py
@@ -5,5 +5,5 @@ import valkey
 class LdapModuleTest(TestCase):
     def test_ldap_auth(self):
         vk = valkey.Valkey(host='localhost', port=6379, db=0)
-        resp = vk.ping()
+        resp = vk.execute_command("LDAP.TEST_AUTH", "user1", "user1@123")
         self.assertTrue(resp)
--- test/integration/test_sanity.py
+++ test/integration/test_sanity.py
@@ -1,5 +1,12 @@
 from unittest import TestCase
 
+import valkey
+
 class SanityTest(TestCase):
     def test_always_passes(self):
         self.assertTrue(True)
+
+    def test_ping_server(self):
+        vk = valkey.Valkey(host='localhost', port=6379, db=0)
+        resp = vk.ping()
+        self.assertTrue(resp)
